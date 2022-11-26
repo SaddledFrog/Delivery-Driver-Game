d60def17214378d46110f51e83ebf4b555d377e8
Oleksii Remizov <saddledfroggd@gmail.com>
2022-11-26 15:47:28 +0200

    add car control

---
+++ Delivery Driver/Assets/Scripts/Driver.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Driver : MonoBehaviour
+{
+    [SerializeField] float steerSpeed = 1f;
+    [SerializeField] float moveSpeed = 0.01f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float streerAmout = Input.GetAxis("Horizontal") * steerSpeed;
+        float moveAmout = Input.GetAxis("Vertical") * moveSpeed;
+        transform.Rotate(0, 0, -streerAmout);
+        transform.Translate(0, moveAmout, 0);
+    }
+}
